f9ce8b3f229dd549338b5b28e7794ff530985b58
Brooke Kullberg <brookekullberg@gmail.com>
2021-05-04 10:30:03 -0700

    remove using directives

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
-using Dealership.Models;
-using Dealership.Models.Helpers;
+// using Dealership.Models;
+// using Dealership.Models.Helpers;
 
 namespace Dealership
 {
